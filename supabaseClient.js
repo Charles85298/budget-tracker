5f5df1e4a3ce30b8e6a595b4f98163e8d2faa7a2
Charles85298 <charles.g.fleming@gmail.com>
2025-09-11 07:49:40 -0700

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
-const supabaseUrl = 'https://gnwfzjdmocorcbyewdcj.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = 'https://gnwfzjdmocorcbyewdcj.supabase.co';
+const supabaseKey = process.env.SUPABASE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey);
 // Make it globally available
 
