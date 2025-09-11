112a350be3a9cdebad4aeaa4074f5011546025f5
Charles85298 <charles.g.fleming@gmail.com>
2025-09-11 07:53:35 -0700

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 const supabaseUrl = 'https://gnwfzjdmocorcbyewdcj.supabase.co';
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdud2Z6amRtb2NvcmNieWV3ZGNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxNjY1MjQsImV4cCI6MjA3Mjc0MjUyNH0.V2j0e-DcWBswTaZnYgtDHMfCHpnBtwlJFWLHKsL25PY"   // <-- paste anon key here
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdud2Z6amRtb2NvcmNieWV3ZGNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxNjY1MjQsImV4cCI6MjA3Mjc0MjUyNH0.V2j0e-DcWBswTaZnYgtDHMfCHpnBtwlJFWLHKsL25PY";   // <-- paste anon key here
 export const supabase = createClient(supabaseUrl, supabaseKey);
 // Make it globally available
 
